9479f8a335620d0f11a9fdd6fe8bb3b2c2a9a73c
Nargis <nargis.salim@btinternet.com>
2021-06-28 20:04:40 +0100

    to fix program currently using the wrong scope for its variables

---
+++ scope.js
@@ -0,0 +1,53 @@
+// Training Days
+// A service called Training Days sends a message for the event signed up for and the days left to train.
+
+// With well scoped variables:- 
+// the getRandEvent() function should select an event at random. 
+// The getTrainingDays() function should return the number of days to train based on the event selected. 
+// The logEvent() and logTime() functions should print the athlete name, event, and number of days to the console.
+
+// The scope of `random` is too loose 
+const random = Math.floor(Math.random() * 3);
+
+const getRandEvent = () => {
+  if (random === 0) {
+    return 'Marathon';
+  } else if (random === 1) {
+    return 'Triathlon';
+  } else if (random === 2) {
+    return 'Pentathlon';
+  }
+};
+
+// The scope of `days` is too tight 
+const getTrainingDays = event => {
+
+  if (event === 'Marathon') {
+    let days = 50;
+  } else if (event === 'Triathlon') {
+    let days = 100;
+  } else if (event === 'Pentathlon') {
+    let days = 200;
+  }
+
+  return days;
+};
+
+// The scope of `name` is too tight 
+const logEvent = event => {
+  const name = 'Nala';
+  console.log(`${name}'s event is: ${event}`);
+};
+
+const logTime = days => {
+  const name = 'Nala';
+  console.log(`${name}'s time to train is: ${days} days`);
+};
+
+const event = getRandEvent();
+const days = getTrainingDays(event);
+// Define a `name` variable. Use it as an argument after updating logEvent and logTime 
+
+
+logEvent(event);
+logTime(days);
